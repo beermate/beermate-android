c945beda7285a1777e55c8b1f4b0bcfbf5ab208e
Bernhard Schussek <bschussek@gmail.com>
2016-05-21 13:05:01 +0200

    Implemented display of different beer fill levels

--- index.android.js
+++ index.android.js
@@ -10,7 +10,9 @@ class BeerMate extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            beerLevel: 0
+            beerLevel: 0,
+            backgroundImage: backgroundImages[5],
+            socketState: 'initial'
         };
     }
 
@@ -18,13 +20,15 @@ class BeerMate extends Component {
         this.socket = new WebSocket('ws://10.0.2.2:2794');
         this.socket.onopen = this.onOpen;
         this.socket.onmessage = this.onMessage;
+        this.socket.onclose = this.onClose;
+        this.socket.onerror = this.onError;
     }
 
     render() {
         return (
-            <Image source={require('./img/full.png')} style={styles.container}>
+            <Image source={this.state.backgroundImage} style={styles.container}>
                 <Text style={styles.instructions}>
-                    Current beer level: {this.state.beerLevel}
+                    Current beer level: {this.state.beerLevel} ({this.state.socketState})
                 </Text>
                 <TouchableHighlight onPress={this.incrBeerLevel}>
                     <View>
@@ -36,19 +40,40 @@ class BeerMate extends Component {
     }
 
     onOpen = () => {
-        this.setState({beerLevel: 1});
+        this.setState({beerLevel: 1, socketState: 'open'});
+    };
+
+    onClose = (event: Event) => {
+        this.setState({socketState: 'closed'});
+    };
+
+    onError = (event: Event) => {
+        this.setState({socketState: 'error'});
     };
 
     onMessage = (event: Event) => {
-        var level = JSON.parse(event.data).level;
+        var beerLevel = JSON.parse(event.data).level;
+        var imageNo = Math.floor(beerLevel * 6);
 
-        this.setState({beerLevel: Math.round(level * 100)});
+        this.setState({
+            beerLevel: Math.round(100 * beerLevel),
+            backgroundImage: backgroundImages[imageNo]
+        });
     };
 
     incrBeerLevel = () => {
         this.socket.send("Test");
     };
 }
+
+const backgroundImages = [
+    require('./img/beer-1.png'),
+    require('./img/beer-2.png'),
+    require('./img/beer-3.png'),
+    require('./img/beer-4.png'),
+    require('./img/beer-5.png'),
+    require('./img/beer-6.png')
+];
 
 const styles = StyleSheet.create({
     container: {
